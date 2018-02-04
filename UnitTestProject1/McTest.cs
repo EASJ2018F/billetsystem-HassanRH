94dec7083adf7bfa93d2d837d5a1374673ee434c
HassanRH <hassan---06@hotmail.com>
2018-02-04 01:14:11 +0100

    Add project files.

---
+++ UnitTestProject1/McTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class McTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MC mc = new MC();
+
+            //assert
+            Assert.AreEqual(mc.Pris(), 125);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            MC mc = new MC();
+
+            //assert
+            Assert.AreEqual(mc.Køretøj(), "MC");
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BilletLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib.BilletLib;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BilTest
+    {
+        [TestMethod]
+        public void Bil_Pris()
+        {
+            //arange
+            Bil bil= new Bil();
+
+            //assert
+            Assert.AreEqual(bil.Pris(),240);
+        }
+        [TestMethod]
+        public void Køretøj()
+        {
+            //arrange
+            Bil bil = new Bil();
+            
+            //assert
+            Assert.AreEqual(bil.køretøj(), "Bil");
+        }
+
+
+    }
+}
