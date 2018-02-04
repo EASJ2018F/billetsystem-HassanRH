e02c7d8ac9721a3015ee114a397c1b0662f3108d
HassanRH <hassan---06@hotmail.com>
2018-02-04 01:38:03 +0100

    ds

---
+++ BilletLibb/Bil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibb
+{
+    public class Bil
+    {
+        public string Nummerplade
+        {
+            get { return Nummerplade; }
+        }
+
+        public DateTime Dato
+        {
+            get { return Dato; }
+        }
+
+        public override int Pris()
+        {
+            return 240;
+        }
+
+        public override string køretøj()
+        {
+            return "Bil";
+        }
+
+
+    }
+}
---
+++ BilletLibb/MC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibb
+{
+    public class MC
+    {
+        public string Nummerplade
+        {
+            get { return Nummerplade; }
+        }
+        public DateTime Dato
+        {
+            get { return Dato; }
+        }
+
+        public int Pris()
+        {
+            return 125;
+        }
+        public string Køretøj()
+        {
+            return "MC";
+        }
+
+
+    }
+}
